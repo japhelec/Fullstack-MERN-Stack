a70dc29f550985cdba3c0c90f9a4b2b8fdab9734
Kuei <material3679@gmail.com>
2020-04-27 00:18:41 +0800

    Google OAuth setup with passport js

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-const express = require("express");  //server side common JS module system
+const express = require("express"); //server side common JS module system
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 
-const app = express();  //start an app, there may be more than one app
+const app = express(); //start an app, there may be more than one app
 
-app.get("/", (req, res) => {            //set up a route handler, focus on HTTP get request
-  res.send({bye: 'buddy'});              //req, request, containing info about client and request
-});                                     //res, respond, tell node server what info to send back to client
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("access token", accessToken);
+      console.log("refresh token", refreshToken);
+      console.log("profile", profile);
+    }
+  )
+);
 
-const PORT = process.env.PORT  || 5000  //Heroku tells us what port to listen 
-app.listen(PORT);                       //tell node to help listen on port PORT
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    //kick in OAuth flow by passport
+    scope: ["profile", "email"],
+  })
+);
 
+app.get("/auth/google/callback", passport.authenticate("google"));
 
+const PORT = process.env.PORT || 5000; //Heroku tells us what port to listen
+app.listen(PORT); //tell node to help listen on port PORT
